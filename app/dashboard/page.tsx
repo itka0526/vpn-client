ba1c475ca51eca8a89bc26c1987e18bc10b734d5
itka0526 <itka0526@gmail.com>
2024-10-26 05:53:00 +0300

    more docs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,26 +7,32 @@ import { Spinner } from "@/components/ui/spinner";
 import { DashboardItemsWrapper } from "./items-wrapper";
 import { Account } from "./account";
 import AccountSkeleton from "./account-skeleton";
+import prisma from "@/lib/db";
 
 export default async function Dashboard() {
     const user = await getSession();
     if (!user.userId) {
         redirect("/login");
+    } else if (user.userId && (await prisma.user.findUnique({ where: { id: 1000 } }))) {
+        // If the user does not exist destroy session
+        (await getSession()).destroy();
+        redirect("/login");
+    } else {
+        return (
+            <>
+                <Suspense fallback={<AccountSkeleton />}>
+                    <Account userId={user.userId} />
+                </Suspense>
+                <Suspense
+                    fallback={
+                        <div className="w-full h-full flex items-center justify-center">
+                            <Spinner />
+                        </div>
+                    }
+                >
+                    <DashboardItemsWrapper userId={user.userId} />
+                </Suspense>
+            </>
+        );
     }
-    return (
-        <>
-            <Suspense fallback={<AccountSkeleton />}>
-                <Account userId={user.userId} />
-            </Suspense>
-            <Suspense
-                fallback={
-                    <div className="w-full h-full flex items-center justify-center">
-                        <Spinner />
-                    </div>
-                }
-            >
-                <DashboardItemsWrapper userId={user.userId} />
-            </Suspense>
-        </>
-    );
 }
--- lib/session-server.ts
+++ lib/session-server.ts
@@ -1,15 +1,8 @@
-"use server";
-
 import { cookies } from "next/headers";
 import { SessionData, sessionOptions } from "./types";
 import { getIronSession } from "iron-session";
-import prisma from "./db";
 
 export async function getSession() {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-    // If the user in db does not exist remove the cookie
-    if (session.userId && !(await prisma.user.findUnique({ where: { id: session.userId } }))) {
-        session.destroy();
-    }
     return session;
 }
