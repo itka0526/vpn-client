b2ef23f7d1aee97edaf81e67504799c70e9c36b5
itka0526 <itka0526@gmail.com>
2024-10-30 22:14:51 +0300

    hints

--- app/dashboard/key-counter.tsx
+++ app/dashboard/key-counter.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { VPNType } from "@prisma/client";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
 
 type KeyCounterProps = {
     count: number;
@@ -11,7 +14,17 @@ type KeyCounterProps = {
 export default function KeyCounter({ count, max, type }: KeyCounterProps) {
     return (
         <div className="flex gap-4 items-center">
-            <span>{type}</span>
+            <Link
+                href={"/instructions"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:text-blue-300 transition-colors flex items-center min-w-0 max-w-[1000px]"
+            >
+                <Button variant={"link"} size={"icon"}>
+                    <ExternalLink className="h-4 w-4 text-primary-foreground " />
+                </Button>
+                <span className="mr-1 truncate">{type}</span>
+            </Link>
             <span>
                 {count} / {max}
             </span>
